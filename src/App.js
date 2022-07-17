b1b51c4cc5ca7a911a3b97ba917c3496e0cc15b2
Khalilyan <khalilyan1001@mail.ru>
2022-07-18 02:22:16 +0400

    Created Btn functions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Buttons from './Components/Buttons';
 
 function App() {
   const [timer, setTimer] = useState({ms:0, s:0, m:0, h:0})
-
+  const [status,setStatus] = useState(0)
+  const [interv,setInterv] = useState()
   let update_ms = timer.ms,
   update_s = timer.ms,
   update_m = timer.ms,
@@ -28,9 +29,34 @@ function App() {
     update_ms++
     return setTimer({ms:update_ms, s:update_s, m:update_m, h:update_h})
   }
+
+function start(){
+  run();
+  setStatus(1)
+  setInterv(setInterval(run,10))
+ 
+}
+function stop(){
+  clearInterval(interv)
+  setStatus(2)
+}
+function reset(){
+  clearInterval(interv)
+  setStatus(0)
+  setTimer({ms:0, s:0, m:0, h:0})
+}
+ function resume(){
+  setStatus(1)
+  setInterv(setInterval(run,10))
+ }
+ function add(){
+  console.log(update_ms,update_s,update_m,update_h)
+ }
+
   return (
     <div className="App">
       <TimerDisplay ms={timer.ms} s={timer.s} m={timer.m} h={timer.h}/>
+      <Buttons status={status} start={start} pause={stop} resume={resume} reset={reset} add={add}/>
       
     </div>
   );
--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
-export default function Buttons() {
+export default function Buttons({start,reset,pause,status,resume,add}) {
   return (
     <div>
-        <button>Start</button>
-        <button>Pause</button>
-        <button>Add</button>
+        {
+        (status === 0) ? <button onClick={start}>Start</button> : ''
+        }
+        {
+            (status === 1) ? <div>
+            <button onClick={pause}>Pause</button> 
+             <button onClick={reset}>Reset</button>
+             <button onClick={add}>Add</button> 
+            </div> : ''
+        }
+        {
+            (status === 2) ? <div>
+             <button onClick={resume}>Resume</button> 
+             <button onClick={reset}>Reset</button>
+            </div> : ''
+        }
+        
     </div>
   )
 }
